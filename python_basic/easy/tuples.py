cb4bd3234a45e19a0954ad7332ec651021a7ee10
Daniel Stoecklein <daniel.stoecklein@gmail.com>
2022-02-11 14:07:01 +0100

    tuples

--- python_basic/easy/tuples.py
+++ python_basic/easy/tuples.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    n = int(raw_input())
-    integer_list = map(int, raw_input().split())
+    n = int(input())
+    integer_list = map(int, input().split())
     integer_tuple = tuple(integer_list)
     print(hash(integer_tuple))
