99306d6bf267254ca6de9ca2252efe03dbf2ddfc
Daniel Stoecklein <daniel.stoecklein@gmail.com>
2022-02-11 18:42:00 +0100

    designer door mat

--- python_basic/easy/string_formatting.py
+++ python_basic/easy/string_formatting.py
@@ -1,2 +1,18 @@
-print(bin(2))
+def print_formatted(number):
+    # your code goes here
+    padding = len(format(number, "b"))-1
+    for i in range(number):
+        print(
+            format(number),
+            " " * padding,
+            format(number, "o"),
+            " " * padding,
+            format(number, "x"),
+            " " * padding,
+            format(number, "b"),
+        )
+    
 
+if __name__ == '__main__':
+    n = int(17)
+    print_formatted(n)
