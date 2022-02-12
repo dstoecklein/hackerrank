11169fcb94b55b1d4d44375826c2144246c557c1
Daniel Stoecklein <daniel.stoecklein@gmail.com>
2022-02-12 13:28:00 +0100

    day 2 diagonal difference

---
+++ 1_week_prep_kit/day2/diagonal_difference.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    right_diag = 0
+    left_diag = 0
+    i = 0
+    j = len(arr)-1
+    for a in arr:
+        right_diag += a[i]
+        left_diag += a[j]
+        i += 1
+        j -= 1
+    return abs(right_diag - left_diag)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
